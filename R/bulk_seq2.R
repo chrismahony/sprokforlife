9da83bed7e14e2c28e36bc483f7c78c3b846d037
mahonyc <65592074+chrismahony@users.noreply.github.com>
2024-10-31 10:51:04 +0000

    Update bulk_seq2.R

--- R/bulk_seq2.R
+++ R/bulk_seq2.R
@@ -1,146 +1,82 @@
 #' @export
-run_deseq_contrast_heatmap <- function(dds, meta_data, targetvar, p_value_threshold = 0.05, 
-                                       log2fc_threshold = 1, condition_colors = NULL, 
-                                       condition_order = NULL, go_analysis = TRUE, 
-                                       organism = "mm", annotation_gs) {
-  
-  # Generate all pairwise comparisons for the target variable
-  conds <- data.frame(t(combn(unique(as.character(meta_data[[targetvar]])), 2)))
-  
-  # Perform DESeq2 analysis for each comparison and collect results
-  results_df <- apply(conds, 1, function(cp) {
-    print(cp)
-    results_df <- data.frame(DESeq2::results(dds, contrast = c(targetvar, cp[2], cp[1])))
-    results_df[["gene"]] <- rownames(results_df)
-    results_df[["comparison"]] <- paste0(cp[2], "_vs_", cp[1])
-    results_df
-  })
-  
-  # Combine all results into a single dataframe
-  results_all <- Reduce(rbind, results_df)
-  
-  # Filter significant results and calculate a "score" based on log2FC and p-value
-  DEGs_f <- results_all %>%
-    filter(padj < p_value_threshold, abs(log2FoldChange) > log2fc_threshold)%>%
-    mutate(score = log2FoldChange * (-log10(pvalue))) %>%
-    arrange(desc(abs(score))) 
-
-assign("DEGs_f", DEGs_f, envir = .GlobalEnv)
-
-  # Add cluster assignments based on log2FoldChange
-  DEGs_f$cluster <- "NO"
-  DEGs_f$cluster[DEGs_f$log2FoldChange > 0.001] <- "UP"
-  DEGs_f$cluster[DEGs_f$log2FoldChange < -0.001] <- "DOWN"
-
-  # Perform GO analysis if specified
-  go.results <- NULL  # Initialize variable
-  go.results.top <- NULL  # Initialize variable
-  
-  assign("results_all", results_all, envir = .GlobalEnv)
-  
-  
-  if (go_analysis) {
-    # Prepare the unique gene IDs for background
-    FilteredGeneID <- unique(results_all$gene)
-    index <- match(FilteredGeneID, annotation_gs$gene_name)
-    ensemblUni <- annotation_gs$ensembl_id[index]
-    ensemblUni <- na.omit(ensemblUni)
-    
-assign("DEGs_f", DEGs_f, envir = .GlobalEnv)
-
-    # Match the gene names to obtain Ensembl IDs
-    index <- match(DEGs_f$gene, annotation_gs$gene_name)
-    DEGs_f$ensembl <- annotation_gs$ensembl_id[index]
-    DEGs_f <- na.omit(DEGs_f)
-    
-    DEGs_f_2 <- DEGs_f %>% filter(log2FoldChange >0.001)
-
-    # Run GO analysis
-    go.results <- runGO.all(
-      results = DEGs_f_2,
-      background_ids = ensemblUni,
-      gene_id_col = "ensembl",
-      gene_id_type = "ensembl",
-      sample_col = "comparison",
-      p_col = "padj",
-      p_threshold = 0.05,
-      species = organism
-    )
-    
-    # Filter genesets and get top results
-    go.results <- filterGenesets(go.results)
-    go.results.top <- go.results %>% group_by(comparison) %>% top_n(n = 5, -p.val)
-    
-    
-    # Create a dot plot for GO term enrichment
-    sampleEnrichmentDotplot(go.results.top,
-                             selection_col = "description",
-                             selected_genesets = unique(go.results.top$description),
-                             sample_id_col = "comparison",
-                             fill_var = "odds.ratio",
-                             maxl = 50,
-                             title = "GO Term Enrichment",
-                             rotate_sample_labels = TRUE)
-  }
-  
-  # Save the results into the R environment
-  
-  assign("go.results", go.results, envir = .GlobalEnv)
-  assign("go.results.top", go.results.top, envir = .GlobalEnv)
-
-  # Generate a heatmap if more than 10 unique genes are found
-  vsd <- vst(dds, blind = TRUE)
-    gene_filter <- unique(filtered_res$gene)
+run_deseq_contrast_heatmap  <- function (dds, meta_data, targetvar, p_value_threshold = 0.05, 
+    log2fc_threshold = 1, condition_colors = NULL, condition_order = NULL, 
+    go_analysis = TRUE, organism = "mm", annotation_gs) 
+{
+    conds <- data.frame(t(combn(unique(as.character(meta_data[[targetvar]])), 
+        2)))
+    results_df <- apply(conds, 1, function(cp) {
+        print(cp)
+        results_df <- data.frame(DESeq2::results(dds, contrast = c(targetvar, 
+            cp[2], cp[1])))
+        results_df[["gene"]] <- rownames(results_df)
+        results_df[["comparison"]] <- paste0(cp[2], "_vs_", cp[1])
+        results_df
+    })
+    results_all <- Reduce(rbind, results_df)
+    DEGs_f <- results_all %>% filter(padj < p_value_threshold, 
+        abs(log2FoldChange) > log2fc_threshold) %>% mutate(score = log2FoldChange * 
+        (-log10(pvalue))) %>% arrange(desc(abs(score)))
+    assign("DEGs_f", DEGs_f, envir = .GlobalEnv)
+    DEGs_f$cluster <- "NO"
+    DEGs_f$cluster[DEGs_f$log2FoldChange > 0.001] <- "UP"
+    DEGs_f$cluster[DEGs_f$log2FoldChange < -0.001] <- "DOWN"
+    go.results <- NULL
+    go.results.top <- NULL
+    assign("results_all", results_all, envir = .GlobalEnv)
+    if (go_analysis) {
+        FilteredGeneID <- unique(results_all$gene)
+        index <- match(FilteredGeneID, annotation_gs$gene_name)
+        ensemblUni <- annotation_gs$ensembl_id[index]
+        ensemblUni <- na.omit(ensemblUni)
+        assign("DEGs_f", DEGs_f, envir = .GlobalEnv)
+        index <- match(DEGs_f$gene, annotation_gs$gene_name)
+        DEGs_f$ensembl <- annotation_gs$ensembl_id[index]
+        DEGs_f <- na.omit(DEGs_f)
+        DEGs_f_2 <- DEGs_f %>% filter(log2FoldChange > 0.001)
+        go.results <- runGO.all(results = DEGs_f_2, background_ids = ensemblUni, 
+            gene_id_col = "ensembl", gene_id_type = "ensembl", 
+            sample_col = "comparison", p_col = "padj", p_threshold = 0.05, 
+            species = organism)
+        go.results <- filterGenesets(go.results)
+        go.results.top <- go.results %>% group_by(comparison) %>% 
+            top_n(n = 5, -p.val)
+        sampleEnrichmentDotplot(go.results.top, selection_col = "description", 
+            selected_genesets = unique(go.results.top$description), 
+            sample_id_col = "comparison", fill_var = "odds.ratio", 
+            maxl = 50, title = "GO Term Enrichment", rotate_sample_labels = TRUE)
+    }
+    assign("go.results", go.results, envir = .GlobalEnv)
+    assign("go.results.top", go.results.top, envir = .GlobalEnv)
+    vsd <- vst(dds, blind = TRUE)
+    gene_filter <- unique(DEGs_f$gene)
     vsd_mat <- assay(vsd)[rownames(vsd) %in% gene_filter, ]
     scale_sub_vsd <- t(scale(t(vsd_mat)))
-      
-      # Set default colors for condition annotations if not provided
-      if (is.null(condition_colors)) {
-        condition_colors <- RColorBrewer::brewer.pal(n = length(unique(meta_data[[targetvar]])), name = "Set1")
+    if (is.null(condition_colors)) {
+        condition_colors <- RColorBrewer::brewer.pal(n = length(unique(meta_data[[targetvar]])), 
+            name = "Set1")
         names(condition_colors) <- unique(meta_data[[targetvar]])
-      }
-      
-      # Reorder conditions based on user input
-      if (!is.null(condition_order)) {
-        # Ensure the levels are reordered based on the input order
-        meta_data[[targetvar]] <- factor(meta_data[[targetvar]], levels = condition_order)
-        
-        # Get the order of the columns based on the new factor levels
+    }
+    if (!is.null(condition_order)) {
+        meta_data[[targetvar]] <- factor(meta_data[[targetvar]], 
+            levels = condition_order)
         ordered_indices <- order(meta_data[[targetvar]])
-        
-        # Reorder the scaled matrix based on the ordered indices
         scale_sub_vsd <- scale_sub_vsd[, ordered_indices]
-        
-        # Reorder the metadata based on the ordered indices for annotations
         meta_data <- meta_data[ordered_indices, ]
-      }
-      
-      # Define annotation for the condition
-      condition_ann <- meta_data[[targetvar]]
-      
-      # Combine both condition and sample annotations in the top annotation
-      top_annotation <- HeatmapAnnotation(
-        condition = condition_ann,
-        col = list(condition = condition_colors)
-      )
-      
-      assign("scale_sub_vsd", scale_sub_vsd, envir = .GlobalEnv)
-     assign("top_annotation", top_annotation, envir = .GlobalEnv)
-
-
-      
-      # Plot the heatmap with top (condition and sample) annotations
-      Heatmap(scale_sub_vsd,
-              top_annotation = top_annotation,
-              col = colorRamp2(c(-1.5, 0, 1.5), c("blue", "white", "red")),
-              row_names_gp = gpar(fontsize = 4),
-              cluster_columns = FALSE,
-              cluster_rows = TRUE,
-              show_row_names = FALSE,
-              show_column_names = FALSE,
-              border = TRUE)
-      
-      
-      sampleEnrichmentDotplot(go.results.top, selection_col = "description", selected_genesets = unique(go.results.top$description), sample_id_col = "comparison", fill_var = "odds.ratio", maxl=50, title="Go term",rotate_sample_labels = T)
-    
+    }
+    condition_ann <- meta_data[[targetvar]]
+    top_annotation <- HeatmapAnnotation(condition = condition_ann, 
+        col = list(condition = condition_colors))
+    assign("scale_sub_vsd", scale_sub_vsd, envir = .GlobalEnv)
+    assign("top_annotation", top_annotation, envir = .GlobalEnv)
+   
+  print(Heatmap(scale_sub_vsd, top_annotation = top_annotation, col = colorRamp2(c(-1.5, 
+        0, 1.5), c("blue", "white", "red")), row_names_gp = gpar(fontsize = 4), 
+        cluster_columns = FALSE, cluster_rows = TRUE, show_row_names = FALSE, 
+        show_column_names = FALSE, border = TRUE))
+  
+    sampleEnrichmentDotplot(go.results.top, selection_col = "description", 
+        selected_genesets = unique(go.results.top$description), 
+        sample_id_col = "comparison", fill_var = "odds.ratio", 
+        maxl = 50, title = "Go term", rotate_sample_labels = T)
 }
